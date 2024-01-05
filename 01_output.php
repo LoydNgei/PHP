9e947dc2c321521a45a58b421e271e85fbca25d4
Loyd Ngei <loydngei@gmail.com>
2024-01-05 08:44:12 +0300

    Output

--- 01_output.php
+++ 01_output.php
@@ -1,4 +1,19 @@
 <?php
-var_export('Hello');
-
+    echo('Hello123');
 ?>
+
+<!-- NOTE -->
+
+<!--
+    
+echo - Outputs strs, nums, html...
+
+print - Works like echo but can only take in a single arg
+
+print_r() - prints single values and arrays
+
+var_dump() - Returns more info like data type and length
+
+var_export() - Similar to var_dump(), Outputs a str representation of a var
+
+-->
