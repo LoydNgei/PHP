29a430536736fed6c0c36dcaa26cceedaa93a28d
Loyd Ngei <loydngei@gmail.com>
2024-01-05 08:44:59 +0300

    Arays

---
+++ 03_array.php
@@ -0,0 +1,27 @@
+<?php
+// Simple Array
+$numbers = [1, 24, 36, 98];
+$fruits = array('apple', 'orange', 'pear');
+
+// var_dump($numbers);
+
+// echo $fruits[1]
+
+// Associative Array
+
+$colors = [
+    1=>'red',
+    4=>'blue',
+    6=>'green'
+];
+
+// echo 
+
+$person = [
+    'first_name' => 'Loyd',
+    'last_name' => 'Ngei',
+    'email' => 'loydngei@gmail.coom'
+];
+
+echo $person['last_name'];
+?>
