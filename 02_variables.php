eb62d60542cf4da75e61fc9c65e2775182ecefe6
Loyd Ngei <loydngei@gmail.com>
2024-01-05 08:44:38 +0300

    Variables

---
+++ 02_variables.php
@@ -0,0 +1,19 @@
+<?php
+// Variables
+
+$name = 'Moses'; // String
+$age = '59';    // int
+
+// echo "Hello my name is ${name} and I am ${age} years old";
+
+
+
+// Constants - Things that you know are never gonna change
+
+define('HOST', 'localhost');
+define('DB_NAME', 'dev_db');
+
+echo HOST;
+
+echo DB_NAME;
+?>
