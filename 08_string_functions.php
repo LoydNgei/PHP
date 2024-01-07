9dbea33a4e06a4d8783c3af4002dea32331ad59c
Loyd Ngei <loydngei@gmail.com>
2024-01-07 22:43:48 +0300

    String functions

---
+++ 08_string_functions.php
@@ -0,0 +1,48 @@
+<?php
+
+
+$string = 'Hello World' ;
+
+// String length
+echo strlen($string);
+
+// Position of 1st occurence of a substring in a string
+
+echo strpos($string, 'o');
+
+// Position of last occurence of a substring in a string
+
+echo strpos($string, 'o');
+
+
+// Reverse a String
+echo strrev($string);
+
+// Convert all chars to lowercase
+
+echo strtolower($string);
+
+// Convert all chars to uppercase
+
+echo strtoupper($string);
+
+// String replace
+
+echo str_replace('World', 'Everyone', $string);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
