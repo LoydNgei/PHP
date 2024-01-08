5b1dfafa6cdfc58f7e51b5c352a673c11f364380
Loyd Ngei <loydngei@gmail.com>
2024-01-08 16:56:36 +0300

    Sanitize inputs

--- 11_sanitize_inputs.php
+++ 11_sanitize_inputs.php
@@ -1,14 +1,21 @@
 <?php
 
 if(isset($_POST['submit'])) {
-    echo $_POST['name'];
-    echo $_POST['age'];
+    $name = filter_input(INPUT_POST, 'name',
+    FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+    $age = filter_input(INPUT_POST, 'age',
+    FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $name = filter_var($_POST['name'],
+    FILTER_SANITIZE_SPECIAL_CHARS);
+
+    echo $name;
 }
 
 
 ?>
-
-
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
     <div>
