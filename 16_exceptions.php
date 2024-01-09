b053351c7e58c81c87157b93e0b3d43f74a11062
Loyd Ngei <loydngei@gmail.com>
2024-01-09 11:30:33 +0300

    Exceptions

---
+++ 16_exceptions.php
@@ -0,0 +1,28 @@
+<?php
+
+function inverse($x) {
+    if (!$x) {
+        throw new Exception('Division by Zero');
+    }
+
+    return 1/$x;
+}
+
+echo inverse(0);
+
+
+try {
+    echo inverse(5);
+    echo inverse(0);
+} catch(Exception $e) {
+    echo 'Caught Exception', $e->getMessage(), ' ';
+}
+
+
+
+
+
+
+
+echo "Hello World"
+?>
