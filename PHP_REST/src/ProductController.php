007e60132781d467e3056a45caa8e24d84ad4a12
LoydNgei <loydngei@gmail.com>
2024-05-16 10:34:29 +0300

    Added Error handler method

--- PHP_REST/src/ProductController.php
+++ PHP_REST/src/ProductController.php
@@ -22,7 +22,57 @@ class ProductController
 
     private function processResourceRequest(string $method, string $id): void
     {
+        $product = $this->gateway->get($id);
 
+        if (!$product) {
+            http_response_code(404);
+            echo json_encode(["message" => "Product not found"]);
+            return;
+        }
+
+        switch ($method) {
+            case "GET":
+                echo json_encode($product);
+                break;
+
+            case "PATCH":
+                $data = json_decode(file_get_contents("php://input"), true);
+
+                $errors =$this->getValidationErrors($data, false);
+
+                if (!empty($errors)) {
+                    http_response_code(422); 
+                    echo json_encode(["errors" => $errors]);
+                    break;
+                }
+
+
+                $rows = $this->gateway->update($product, $data);
+
+
+                http_response_code(201);
+
+                echo json_encode([
+                    "message" => "Product $id updated",
+                    "rows affected" => $rows
+                ]);
+                break;
+
+            case "DELETE":
+               $rows = $this->gateway->delete($id);
+
+               echo json_encode([
+                "message" => "Product $id deleted",
+                "rows affected" => $rows
+               ]);
+               break;
+
+            default:
+            http_response_code(405);
+            header("Allow: GET, PATCH, DELETE"); 
+        }
+
+        echo json_encode($product);
     }
 
     private function processCollectionRequest(string $method): void
@@ -33,9 +83,47 @@ class ProductController
                 break;
             
             case "POST":
-                $data = $_POST;
+                $data = json_decode(file_get_contents("php://input"), true);
 
-                var_dump($data);
+                $errors =$this->getValidationErrors($data);
+
+                if (!empty($errors)) {
+                    http_response_code(422); 
+                    echo json_encode(["errors" => $errors]);
+                    break;
+                }
+
+
+                $id = $this->gateway->create($data);
+
+
+                http_response_code(201);
+
+                echo json_encode([
+                    "message" => "Product created",
+                    "id" => $id
+                ]);
+                break;
+            default:
+                http_response_code(405);
+                header("Allow: GET, POST");
         }
+    }
+
+    private function getValidationErrors(array $data, bool $is_new = true): array
+    {
+        $error = [];
+
+        if ($is_new && empty($data["name"])) {
+            $errors[] = "name is required";
+        }
+
+        if (array_key_exists("size", $data)) {
+            if (filter_var($data["size"], FILTER_VALIDATE_INT) === false) {
+                $errors[] = "size must be an integer";
+            }
+        }
+
+        return $error;
     }
 }
